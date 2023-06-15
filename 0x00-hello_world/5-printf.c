65257704e2f207766839aaba17de352a4f18187f
Winnie Kiarago <kiaragowinnie@ymail.com>
2023-06-15 12:50:24 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - printing in c programming using print f fn
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
