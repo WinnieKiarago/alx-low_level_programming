9055ef3cbb90699c3e42054c1b4f3b6d31063ee3
Winnie Kiarago <kiaragowinnie@ymail.com>
2023-06-19 14:17:21 -0700

    omit some

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,17 +6,17 @@
 */
 int main(void)
 {
-        char c;
-
-        c = 'a';
-        while
-                (c <= 'z')
-                {
-		if (( c != 'q' && c != 'e') && c <= 'z')
-                        putchar (c);
-		c++;
-                }
-        putchar('\n');
-        return (0);
+	char c;
+	
+	c = 'a';
+	while
+		(c <= 'z')
+		{
+			if (( c != 'q' && c != 'e') && c <= 'z')
+				putchar (c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
 }
 
