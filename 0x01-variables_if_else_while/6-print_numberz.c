57442e124681d96daa919c06e8bde49cdadf5d23
Winnie Kiarago <kiaragowinnie@ymail.com>
2023-06-19 15:19:11 -0700

    no printf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program that prints single numbers of base 10 starting from 0
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+        for (a = 0; a < 10; a++)
+		putchar (a);
+	putchar('\n');
+        return (0);
+}
