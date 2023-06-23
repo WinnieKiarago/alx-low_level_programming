1fa940c14155364e98a231785358fbd8d95199df
Winnie Kiarago <kiaragowinnie@ymail.com>
2023-06-23 12:01:08 -0700

    0x03-debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * main - Test function for positive or negative
@@ -11,6 +11,5 @@ int main(void)
 	
 	i = 0;
 	positive_or_negative(i);
-	
 	return (0);
 }
