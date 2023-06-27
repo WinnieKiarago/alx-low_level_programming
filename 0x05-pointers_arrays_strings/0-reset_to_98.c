693602fd067147c4fe7b43d7f47e532ecf4486eb
Winnie Kiarago <kiaragowinnie@ymail.com>
2023-06-26 23:58:41 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - 98 Battery st.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int var = 98;
+	int *ip;
+
+	putchar("Value of *ip variable: %d\n", *ip);
+	return (0);
+}
