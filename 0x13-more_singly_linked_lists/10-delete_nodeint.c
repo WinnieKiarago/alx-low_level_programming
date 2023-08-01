8a9266131fe76e914b390fbf07e197096cb22c20
WinnieKiarago <kiaragowinnie@ymail.com>
2023-08-01 14:24:00 -0700

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listsint_t *temp = *head;
+	listint_t *temp = *head;
 	listint_t *current = NULL;
 	unsigned int count = 0;
 
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (count < index  - 1)
+	while (count < index - 1)
 	{
 		if (!temp || !(temp->next))
 			return (-1);
@@ -30,8 +30,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		count++;
 	}
 
+
 	current = temp->next;
 	temp->next = current->next;
 	free(current);
+
 	return (1);
 }
