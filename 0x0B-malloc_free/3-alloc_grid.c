902b4423352e5330add504379dee615c9694137f
Winnie Kiarago <kiaragowinnie@ymail.com>
2023-09-06 03:28:23 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -35,3 +35,31 @@ int **alloc_grid(int width, int height)
 	}
 	return (grid);
 }
+int main(void)
+{
+	int width = 4;
+	int height = 3;
+
+	int **myGrid = alloc_grid(width, height);
+
+	if (myGrid != NULL)
+	{
+		myGrid[1][2] = 42;
+
+		for (int i = 0; i < height; i++)
+		{
+			free(myGrid[i]);
+		}
+		free(myGrid);
+	}
+	else
+	{
+		char msg[] = "Memory allocation failed.\n";
+
+		for (int i = 0; msg[i] != '\0'; i++)
+		{
+			_putchar(msg[i]);
+		}
+	}
+	return (0);
+}
