b420211d28e4e74924f904e297c61607c9ce306e
Winnie Kiarago <kiaragowinnie@ymail.com>
2023-09-06 01:56:52 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@ char *str_concat(char *s1, char *s2)
 	char *concatenated;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	concatenated = (char *)malloc((len1 + len2 + 1) * sizeof(char));
