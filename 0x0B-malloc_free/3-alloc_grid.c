38021a4f3bcc7bcda714232bd90c2370443b06d6
Winnie Kiarago <kiaragowinnie@ymail.com>
2023-09-06 03:41:11 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
-			for (j = 0; j >= 0; j--)
+			for (; j >= 0; j--)
 				free(grid[j]);
 			free(grid);
 			return (NULL);
