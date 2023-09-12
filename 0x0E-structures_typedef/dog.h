45bcbbf7ed27b44ed905c570f41d7cf36d792de1
Winnie Kiarago <kiaragowinnie@ymail.com>
2023-09-12 12:47:17 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure declaration
+ * @name: element
+ * @age: element
+ * @owner: element
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
