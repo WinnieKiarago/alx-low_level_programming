8ecffeabd89631c121a52a740aa3ab07737aa6b2
Winnie Kiarago <kiaragowinnie@ymail.com>
2023-09-12 13:32:19 -0700

    friendly dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
 /**
  * init_dog - initializer
  * @d: pointer
@@ -11,7 +12,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		d = malloc(sizeof(struct dog));
-	d->name = name;
+	d->name = strdup(name);
 	d->age = age;
-	d->owner = owner;
+	d->owner = strdup(owner);
 }
