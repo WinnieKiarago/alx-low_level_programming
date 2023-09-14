a3b1e5d5f3c1235a18f3ebdc8bcd2182fb7df532
Winnie Kiarago <kiaragowinnie@ymail.com>
2023-09-14 11:46:08 -0700

    sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,15 +9,16 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum;
+	unsigned int i;
+	int sum = 0;
 	va_list ap;
 
 	va_start(ap, n);
 
 	if (n == 0)
 		return (0);
-	sum = 0;
-	for (unsigned int i = 0; i < n; i++)
+
+	for (i = 0; i < n; ++i)
 		sum += va_arg(ap, int);
 
 	va_end(ap);
